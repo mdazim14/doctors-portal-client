e651d962562d4d56e35d5de0a76c714ea6309705
mdazim14 <mdazim.cse@gmail.com>
2022-01-04 12:36:54 +0600

    appoinment img

--- src/components/Home/MakeAppoinment/MakeAppoinment.js
+++ src/components/Home/MakeAppoinment/MakeAppoinment.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import Doctor from '../../../images/doctor.png';
+import './MakeAppoinment.css';
 
 const MakeAppoinment = () => {
-    const styles = {
-        height: '410px',
-    }
-    const styleImg = {
-        position: 'relative',
-        top: '-63px',
-        height: '535px'
-    }
+    
     return (
-        <main className="m-5">
-
-            <div style={styles} className="text-center m-5  d-flex align-items-center bg-info">
+        <main className="">
+            <div className="make-appoinment m-auto row">
                 <div className="col-md-6 text-end">
-                    <img style={styleImg} src={Doctor} alt="" />
+                    <img className="appoinmentImg" src={Doctor} alt="" />
                 </div>
-                <div className="col-md-6 text-start">
+                <div className=" appoinment-text col-md-6 p-5 text-start">
                     <h5 className="mb-5">Appointment</h5>
                     <h1>Make an Appoinment</h1>
                     <h1 className="mb-4">Today</h1>
